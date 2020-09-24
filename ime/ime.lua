b52a3db8417b20266db84b2b949baaa6d502c252
nujabse <h.p.zhumeng@gmail.com>
2020-09-24 16:04:06 +0800

    Updated ime switcher for better use of rime on Mac OS

--- ime/ime.lua
+++ ime/ime.lua
@@ -1,65 +1,51 @@
 local function Chinese()
-    hs.keycodes.currentSourceID("com.sogou.inputmethod.sogou.pinyin")
+    hs.keycodes.currentSourceID("im.rime.inputmethod.Squirrel.Rime")
 end
 
---Chinese()
--- --local function Chinese()
--- --    hs.keycodes.currentSourceID("com.baidu.inputmethod.BaiduIM.pinyin")
--- --end
-
--- local function English()
---     hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
--- end
+local function English()
+    -- ABC
+    hs.keycodes.currentSourceID("com.apple.keylayout.US")
+end
 
 -- app to expected ime config
+-- app和对应的输入法
 local app2Ime = {
-    {'/Applications/iTerm.app', 'Chinese'},
-    {'/Applications/Xcode.app', 'Chinese'},
-    {'/Applications/Google Chrome.app', 'Chinese'},
-    {'/System/Library/CoreServices/Finder.app', 'Chinese'},
-    {'/Applications/DingTalk.app', 'Chinese'},
-    {'/Applications/Kindle.app', 'Chinese'},
-    {'/Applications/NeteaseMusic.app', 'Chinese'},
+    {'/Applications/QQ.app', 'Chinese'},
     {'/Applications/WeChat.app', 'Chinese'},
-	{'/Applications/QQ.app', "Chinese"},
-	{'/Applications/Safari.app',"Chinese"},
-	{'/Applications/VNote.app','Chinese'},
-    {'/Applications/System Preferences.app', 'Chinese'},
-    {'/Applications/Dash.app', 'Chinese'},
-    {'/Applications/MindNode.app', 'Chinese'},
-    {'/Applications/Preview.app', 'Chinese'},
-    {'/Applications/wechatwebdevtools.app', 'Chinese'},
-    {'/Applications/Sketch.app', 'Chinese'},
-    {'/Applications/Sublime Text.app', 'Chinese'},
-    {'/Applications/MarginNote 3.app', 'Chinese'},
-    {'/Applications/PyCharm.app', 'Chinese'},
-	{'/Applications/Microsoft OneNote.app', 'Chinese'},
-    {'/Applications/SSH Sheel.app', 'Chinese'},
-    {'/Applications/Mate Translate.app', 'Chinese'},
-    {'Applications/Notion.app','Chinese'}
+    {'/Applications/Evernote.app', 'Chinese'},
+    {'/Applications/NeteaseMusic.app', 'Chinese'},
+    {'/Applications/iTerm.app', 'English'},
+    {'/Applications/Visual Studio Code.app', 'English'},
+    {'/Applications/Xcode.app', 'English'},
+    {'/Applications/Google Chrome.app', 'English'},
+    {'/System/Library/CoreServices/Finder.app', 'English'},
+    {'/Applications/Kindle.app', 'English'},
+    {'/Applications/System Preferences.app', 'English'},
+    {'/Applications/DingTalk.app', 'Chinese'},
 }
 
 function updateFocusAppInputMethod()
-    -- local ime = 'English'
-    -- local focusAppPath = hs.window.frontmostWindow():application():path()
-    -- for index, app in pairs(app2Ime) do
-    --     local appPath = app[1]
-    --     local expectedIme = app[2]
+    local ime = 'English'
+    local focusAppPath = hs.window.frontmostWindow():application():path()
+    for index, app in pairs(app2Ime) do
+        local appPath = app[1]
+        local expectedIme = app[2]
 
-    --     if focusAppPath == appPath then
-    --         ime = expectedIme
-    --         break
-    --     end
-    -- end
+        if focusAppPath == appPath then
+            ime = expectedIme
+            break
+        end
+    end
 
-    -- if ime == 'English' then
-    --    English()
-    -- else
+    if ime == 'English' then
+        English()
+    else
         Chinese()
-    -- end
+    end
 end
 
 -- helper hotkey to figure out the app path and name of current focused window
+-- 当选中某窗口按下ctrl+command+.时会显示应用的路径等信息
 hs.hotkey.bind({'ctrl', 'cmd'}, ".", function()
     hs.alert.show("App path:        "
     ..hs.window.focusedWindow():application():path()
@@ -72,6 +58,7 @@ hs.hotkey.bind({'ctrl', 'cmd'}, ".", function()
 end)
 
 -- Handle cursor focus and application's screen manage.
+-- 窗口激活时自动切换输入法
 function applicationWatcher(appName, eventType, appObject)
     if (eventType == hs.application.watcher.activated) then
         updateFocusAppInputMethod()
